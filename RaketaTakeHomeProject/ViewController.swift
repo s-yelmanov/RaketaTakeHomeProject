3db3a19b2f9721cfecc8fb7409dfe207c18f86bd
Sergey Yelmanov <sergey.yelmanov@gmail.com>
2020-06-13 13:00:25 +0300

    Initial Commit

---
+++ RaketaTakeHomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RaketaTakeHomeProject
+//
+//  Created by Sergey Yelmanov on 13.06.2020.
+//  Copyright © 2020 Sergey Yelmanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
